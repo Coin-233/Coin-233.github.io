4274aef1bb18349e38ed0157734ae5e4f685705e
Coin-233 <53845501+Coin-233@users.noreply.github.com>
2021-06-13 15:16:12 +0800

    Add files via upload

---
+++ server/js/window.js
@@ -0,0 +1,16 @@
+document.getElementById('zhezhao').style.display = "none";
+function dianwo() {
+    document.getElementById('zhezhao').style.display = "";
+}
+function hidder() {
+    document.getElementById('zhezhao').style.display = "none";
+}
+function wx_pay() {
+    document.getElementById("pay_img").src="https://lablog-images.oss-cn-beijing.aliyuncs.com/images/wchat.png"
+}
+function zfb_pay() {
+    document.getElementById("pay_img").src="https://lablog-images.oss-cn-beijing.aliyuncs.com/images/pay.jpg"
+}
+function qq_pay() {
+    document.getElementById("pay_img").src="https://lablog-images.oss-cn-beijing.aliyuncs.com/images/QQ.png"
+}
